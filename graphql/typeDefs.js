38db2b6ee0f3d9dda980585efb6ef62dfdfb3cd5
3madov-77 <emadzxy7@gmail.com>
2021-07-17 21:26:07 +0300

    added register new user

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -8,7 +8,23 @@ module.exports = gql`
         username: String!
         createdAt: String!
     }
+    type User {
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        created: String!
+    }
+    input RegisterInput {
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
     type Query {
         getPosts: [Post]
+    }
+    type Mutation {
+        register(registerInput: RegisterInput): User!
     }
 `;
