0f31c5da91c6e044ee04d855fe26a4e3d4806cea
3madov-77 <emadzxy7@gmail.com>
2021-07-17 16:22:26 +0300

    added connection to MDB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const { gql } = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config')
 
 const typeDefs = gql`
     type Query {
@@ -18,6 +21,11 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port:5000 }).then((res) =>{
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log('MongoDB Connected..');
+    return server.listen({ port:5000 })
+})
+.then((res) =>{
     console.log(`Our Server are runing in ${res.url}`);
 });
