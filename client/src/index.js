4788c28e74dfd8a36cd58f60c2b0d5106e7a81d7
3madov-77 <emadzxy7@gmail.com>
2021-07-20 02:53:49 +0300

    installing more requierd dependencys

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import ApolloProvider from './ApolloProvider';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
@@ -7,4 +7,4 @@ ReactDOM.render(ApolloProvider, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
