a7f7d863b165a4aa71a597f96513c9abda15fae3
3madov-77 <emadzxy7@gmail.com>
2021-07-19 14:37:42 +0300

    implementing delete comment for any post

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -24,6 +24,20 @@ module.exports = {
                 })
                 await post.save();
                 return post;
+            } else throw new UserInputError('Post not found!')
+        },
+        async deleteComment(_, { postId, commentId }, context){
+            const { username } = checkAuth(context);
+            
+            const post = await Post.findById(postId);
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentId, 1);
+                    await post.save();
+                    return post;
+                } else throw new AuthenticationError('Action not allowed')
             } else throw new UserInputError('Post not found!')
         }
     }
