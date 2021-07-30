7b76139adbf18c9b49e75cafa96a3a711d877de2
3madov-77 <emadzxy7@gmail.com>
2021-07-30 18:36:19 +0300

    added popup to delete button

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -4,6 +4,7 @@ import { useMutation } from "@apollo/react-hooks";
 import { Button, Confirm, Icon } from 'semantic-ui-react';
 
 import { FETCH_POSTS_QUERY } from '../util/graphql'
+import MyPopups from '../util/MyPopups'
 
 function DeleteButton({ postId, commentId, callback }){
     const [confirmOpen, setConfirmOpen] = useState(false);
@@ -33,14 +34,16 @@ function DeleteButton({ postId, commentId, callback }){
 
     return (
     <>
-        <Button
-            as='div'
-            color='red'
-            floated='right'
-            onClick={() => setConfirmOpen(true)}
-        >
-            <Icon name='trash' style={{ margin: 0 }} />
-        </Button>
+        <MyPopups content={commentId ? 'Delete comment' : 'Delete Post'}>
+            <Button
+                as='div'
+                color='red'
+                floated='right'
+                onClick={() => setConfirmOpen(true)}
+            >
+                <Icon name='trash' style={{ margin: 0 }} />
+            </Button>
+        </MyPopups>
         <Confirm
             open={confirmOpen}
             onCancel={() => setConfirmOpen(false)}
